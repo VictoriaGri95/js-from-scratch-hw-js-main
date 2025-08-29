d71040a7c371508ea3aab2e52305fa0a1e2d7a4b
VictoriaGri95 <v.grivusevich95@gmail.com>
2025-08-29 09:42:23 +0300

    lesson-03 all tasks

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,9 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage() {}
+function calculateAverage(number_1, number_2) {
+  let arithmeticMean = (number_1 + number_2) / 2
+  return arithmeticMean;
+}
+let result = calculateAverage(10, 10)
+console.log(result)
+
--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,15 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
-function findLargest() {}
+function findLargest(number_1, number_2, number_3) {
+
+  if  (number_1 >= number_2 && number_1 >= number_3) {
+    return number_1;
+  } else if (number_2 >= number_1 && number_2 >= number_3) {
+    return number_2;
+  } else {
+    return number_3;
+  }
+}
+    let result = findLargest(7, 0, 1)
+    console.log(result)
+
