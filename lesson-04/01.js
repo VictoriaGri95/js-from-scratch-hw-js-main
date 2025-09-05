66e6e15b5d7a591ce751e042e160fdab2b5d8deb
VictoriaGri95 <v.grivusevich95@gmail.com>
2025-09-05 13:55:17 +0300

    lesson-04 task-01

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -13,4 +13,14 @@
 includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 */
 
-function includesElement() {}
+function includesElement(array, element) {
+  for (let i = 0; i < array.length; i++) {
+    let item = array[i]
+    if (item === element) {
+      return true
+    }
+  }
+  return false
+}
+
+console.log(includesElement([1, 2, 3], 2))
